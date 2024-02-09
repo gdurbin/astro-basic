6377bc3c673d23f6c9a8e659e8fb262931bbcac4
Gerrett Durbin <gerrettdurbin@gmail.com>
2024-02-09 13:56:21 -0800

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "",
+  site: "https://astro-basic.pages.dev",
   integrations: [
     tailwind(),
     icon({
